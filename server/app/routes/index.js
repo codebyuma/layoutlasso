6ba27afbffef5c400469e254943d8ade7e65e2ec
NicholasJV <nick@Nicks-MBP.local>
2015-11-23 17:46:08 -0500

    test

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -3,9 +3,10 @@ var router = require('express').Router();
 module.exports = router;
 
 router.use('/members', require('./members'));
-
+// hi
 // Make sure this is after all of
 // the registered routes!
 router.use(function (req, res) {
     res.status(404).end();
 });
+ 
