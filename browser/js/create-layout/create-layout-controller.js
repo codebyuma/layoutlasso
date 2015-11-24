76229587798a2c697bb7320d556ef01a902f5da7
Sunil Unka <sunil.unka@gmail.com>
2015-11-24 18:31:30 -0500

    Pushing up build with gridstack-angular added to the build.

--- browser/js/create-layout/create-layout-controller.js
+++ browser/js/create-layout/create-layout-controller.js
@@ -1,22 +1,54 @@
 app.controller("CreateLayoutCtrl", function($scope){
 
-  var options = {
-      cell_height: 80,
-      vertical_margin: 0,
-      animate: false,
-      margin: 0
+  $scope.options = {
+    cell_height: 60,
+    vertical_margin: 0
+  }
+
+  $scope.widgets = [{x: 0, y: 0, width: 1, height: 1}];
+
+  $scope.addWidget = function() {
+      var newWidget = { x:0, y:0, width:1, height:1 };
+      $scope.widgets.push(newWidget);
   };
 
+  $scope.removeWidget = function(w) {
+      var index = $scope.widgets.indexOf(w);
+      $scope.widgets.splice(index, 1);
+  };
 
-  var grid = $('.grid-stack').gridstack(options).data('gridstack');
-  console.log(grid);
-
-
-
-  $scope.addNewGridElement = function(){
-    console.log(grid);
-    var el = $("<div>").addClass("new-element");
-    grid.add_widget(el, 200, 200, 50, 50, true)
-  }
+  // var options = {
+  //     cell_height: 80,
+  //     vertical_margin: 0,
+  //     animate: false,
+  //     margin: 0
+  // };
+  //
+  // function addItemsToGrid(items){
+  //   items.forEach(function(item){
+  //     grid.add_widget(item, item.x, item.y, item.width, item.height);
+  //   })
+  // }
+  //
+  //
+  // var grid = $('.grid-stack').gridstack(options).data('gridstack');
+  // console.log(grid);
+  //
+  // $('.grid-stack').on('change', function(event, items){
+  //   $scope.gridElements = items;
+  //   addItemsToGrid($scope.gridElements);
+  // })
+  //
+  // $scope.grid = grid;
+  //
+  // $scope.gridElements = [];
+  //
+  // $scope.addNewGridElement = function(){
+  //   console.log(grid);
+  //
+  //   var el = $("<div></div>").addClass("new-element");
+  //   grid.add_widget(el, 0, 0, 1, 1, false);
+  //
+  // }
 
 })
--- server/app/configure/static-middleware.js
+++ server/app/configure/static-middleware.js
@@ -8,6 +8,7 @@ module.exports = function (app) {
     var root = app.getValue('projectRoot');
 
     var npmPath = path.join(root, './node_modules');
+    var bowerPath = path.join(root, './bower_components');
     var publicPath = path.join(root, './public');
     var browserPath = path.join(root, './browser');
 
@@ -15,5 +16,6 @@ module.exports = function (app) {
     app.use(express.static(npmPath));
     app.use(express.static(publicPath));
     app.use(express.static(browserPath));
+    app.use(express.static(bowerPath));
 
 };
