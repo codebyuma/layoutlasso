0d8cf1cb02b4a08e27fdd809f35d4f341ef5c850
Sunil Unka <sunil.unka@gmail.com>
2015-11-23 22:30:48 -0500

    Working on basic integration of gridster. Also fixed some styling of navbar.

--- browser/js/create-layout/create-layout-controller.js
+++ browser/js/create-layout/create-layout-controller.js
@@ -1,3 +1,25 @@
 app.controller("CreateLayoutCtrl", function($scope){
-  
+
+  var mainGrid = $(".gridster").gridster({
+    widget_margins: [0, 0],
+    widget_base_dimensions: [100, 100],
+    max_cols: 10,
+    min_cols: 10,
+    min_rows: 15,
+    max_size_x: true,
+    autogenerate_stylesheet: true,
+    resize: {
+      enabled: true,
+      axes: ["both"],
+
+    },
+    avoid_overlapped_widgets: true
+  }).data("gridster");
+
+  console.log(mainGrid);
+
+
+  $scope.addNewGridElement = function(){
+    mainGrid.add_widget("<div class='new new-element'><p>Here is a new widget!</p></div>", 10, 1)
+  }
 })
