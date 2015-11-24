8872e49460f929abc74fd984a103c50afd1cc8f2
Sunil Unka <sunil.unka@gmail.com>
2015-11-24 16:24:49 -0500

    Working on creating new elements.

--- browser/js/create-layout/create-layout-controller.js
+++ browser/js/create-layout/create-layout-controller.js
@@ -1,19 +1,22 @@
 app.controller("CreateLayoutCtrl", function($scope){
 
-  $(function () {
-      var options = {
-          cell_height: 80,
-          vertical_margin: 10
-      };
-      $('.grid-stack').gridstack(options);
-  });
+  var options = {
+      cell_height: 80,
+      vertical_margin: 0,
+      animate: false,
+      margin: 0
+  };
 
-  //
-  // var options = {
-  //     cell_height: 80,
-  //     vertical_margin: 10,
-  //     height: 15
-  // }
-  //
-  // console.log("GRIDSTACK", $('.grid-stack').gridstack(options));
+
+  var grid = $('.grid-stack').gridstack(options).data('gridstack');
+  console.log(grid);
+
+
+
+  $scope.addNewGridElement = function(){
+    console.log(grid);
+    var el = $("<div>").addClass("new-element");
+    grid.add_widget(el, 200, 200, 50, 50, true)
+  }
+
 })
