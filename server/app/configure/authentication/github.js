0301847de5dcf5f07e52771da2ea22c2d626f76d
Uma Chandran <umachandran6@gmail.com>
2015-11-25 10:15:42 -0500

    updates

--- server/app/configure/authentication/github.js
+++ server/app/configure/authentication/github.js
@@ -17,6 +17,7 @@ module.exports = function (app) {
 
     var verifyCallback = function (accessToken, refreshToken, profile, done) {
         console.log("in the github verify callback", profile);
+        
         UserModel.findOne({ 'github.id': profile.id }).exec() 
             .then(function (user) {
                 if (user) {
