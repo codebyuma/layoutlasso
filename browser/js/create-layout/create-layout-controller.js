1b8e41b2fd29b955241255e01dc068c11493d259
Uma Chandran <umachandran6@gmail.com>
2015-11-25 17:07:10 -0500

    be careful with counter

--- browser/js/create-layout/create-layout-controller.js
+++ browser/js/create-layout/create-layout-controller.js
@@ -18,7 +18,7 @@ app.controller("CreateLayoutCtrl", function($scope, $compile){
   $scope.counter = 0;
 
   $scope.addNewGridElement = function(){
-    $scope.counter++;
+    $scope.counter++; // this may be a problem when we load in a saved grid and remove and add - may have multiple with the same id
     var el = $compile("<div class='grid-stack-item' id=" + $scope.counter + "><div class='grid-stack-item-content new-element'><button ng-click='removeWidget(" + $scope.counter + ")'> {{ text }} </button></div></div>")($scope);
     var newWidget = grid.add_widget(el, 0, 0, 1, 1, true);
 
