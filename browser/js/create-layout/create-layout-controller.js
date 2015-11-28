e1a57b25b9417468e69322d1bfec5263f79645a9
Sarah Edkins <edkins.sarah@gmail.com>
2015-11-28 10:29:28 -0500

    Widgets print to Export View, but nested widgets print inside original grid - still working on this

--- browser/js/create-layout/create-layout-controller.js
+++ browser/js/create-layout/create-layout-controller.js
@@ -78,6 +78,7 @@ var createElement = function(id, content) {
   // This exportation uses the Gridstack system and requires a bower-install of Gridstack (not Bootstrap).
 
   $scope.convertedHTML = "";
+  $scope.exportGrids = {};
   var userContentRegex = /<div class="lasso-user-content">[\s\S]*?<\/div><div class="lasso-end-user-content"><\/div>/im;
 
   // static grid that does not allow modification
@@ -86,7 +87,7 @@ var createElement = function(id, content) {
   };
 
   $scope.export_grid = $('#export-grid').gridstack(export_options).data('gridstack');
-  console.log("$scope.export_grid", $scope.export_grid);
+  $scope.exportGrids["export-grid"] = $scope.export_grid;
 
   // helper function to create a new EXPORTABLE element - not the same as our internal lasso elements
   var createExportElement = function(id, content) {
@@ -98,18 +99,41 @@ var createElement = function(id, content) {
   }
 
 // adds a new EXPORTABLE grid to the export_grid - a static grid that cannot be modified in Layout Lasso
-  var addExportWidget = function(grid, id, x, y, w, h){
+  var addExportWidget = function(grid, id, content, x, y, w, h){
     grid = grid || $scope.export_grid;
     $scope.counter++; // this may be a problem when we load in a saved grid and remove and add - may have multiple with the same id
-    var el = createElement(id);
+    var el = createExportElement(id, content);
     var newWidget = grid.add_widget(el, x, y, w, h, false);
   }
 
+  var addNestedExportGrid = function(thisWidgetId, gridId){
+      var thisWidget = $('#' + thisWidgetId);
+
+      // make selected widget into a grid
+      var newGridID = "grid" + gridId;
+      thisWidget.append($compile("<div class='grid-stack grid-stack-nested' id='" +
+      newGridID+ "'></div>")($scope));
+
+      // save the new grid to exportGrids object on the $scope, and return it
+      var newGrid = $('#' + newGridID).gridstack(export_options).data('gridstack');
+      $scope.exportGrids[newGridID] = newGrid;
+      return newGrid;
+  }
+
   $scope.convertToHTML = function() {
+// parent is a GRID with the add_widget method on it, and w,h  (not x,y)
+// nodes are the widgets on the parent grid, and have x,y,w,h on them
+
     console.log("button clicked!");
     var nodes, parent;
     for(var key in $scope.nestedGrids) {  // for each grid
-      parent = $scope.nestedGrids[key];
+
+      if(key == "main-grid") {   // main grid becomes export grid
+        parent = $scope.export_grid;
+      } else { // parent grid should be a new nested grid on export grid
+        // parent = addNestedExportGrid();
+        parent = $scope.nestedGrids[key];
+      }
       console.log("parent", parent);
       console.log("parent.grid", parent.grid);
       nodes = $scope.nestedGrids[key].grid.nodes;
@@ -118,9 +142,9 @@ var createElement = function(id, content) {
          if (matches.length == 0) {
            throw new Error("Error converting to html - No user content found.");
          } else {
-           // create a new grid element matching the properties of that one
+           // create a new grid element matching the properties of that one to put on $scope.export_grid
           console.log("content:", matches[0]);
-
+          addExportWidget(parent, nodes[i]._id, matches[0], nodes[i].x, nodes[i].y, nodes[i].width, nodes[i].height);
          }
       }
     }
