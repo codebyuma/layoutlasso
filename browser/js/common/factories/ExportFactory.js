b594448007b753cf71bd0814b498241095622d45
Sarah Edkins <edkins.sarah@gmail.com>
2015-11-30 18:23:03 -0500

    generateRow function works, except for empty cols

--- browser/js/common/factories/ExportFactory.js
+++ browser/js/common/factories/ExportFactory.js
@@ -39,38 +39,52 @@ app.factory('ExportFactory', function(GridFactory) {
 
     ExportFactory.convertToHTML = function() {
       console.log("button clicked!");
-
+          var html = "";
+          var node;
           var parentObj = makeParentObject();
           console.log("parentObj", parentObj);
           for (var key in parentObj) {  // for each parent
               // use generateHTML function to build
               // grid system for each ROW
               // and each parent grid
+              console.log("parentObj[key]", parentObj[key]);
+              console.log("GridFactory.nestedGrids[key]", GridFactory.nestedGrids[key]);
+
+              generateRow(html, parentObj[key]);
           }
 
       };
 
-  function generateHTML(newHTML, r, c, content, sz){
+    function getNumCols() {
+      // look at all the nodes in the grid
+      // their widths will add up to 12.
+      //
+    }
 
+  function generateHTML(newHTML, r, c, content, sz){
     var sz = sz || "md";
     var rows = r, cols = c;
-    var span = 0; // number of Bootstrap columns to span across
+    var span = 12 / cols; // number of Bootstrap columns to span across
 
     // generate the html
     newHTML += bits.container;
     for (var i = 0; i < rows; i++) {
         newHTML += bits.row;
-        for (var j = 0; j < cols; j++){
-            // make column
-            newHTML += colMaker(sz, span);
-            // add content to column
-            newHTML += content;
-            // close column
-            newHTML += bits.close;
-        } // close row
+
         newHTML += bits.close;
     } // close container
     newHTML += bits.close;
+  }
+
+  function generateRow(html, nodesArr, size) {
+    var sz = size || "md";
+    for (var j = 0; j < nodesArr.length; j++){
+        html += colMaker(sz, nodesArr[j].width); // make column
+        html += nodesArr[j].content;  // add content to column
+        html += bits.close; // close column
+    }
+    console.log("html from generateRow() is", html);
+    return html;
   }
 
   return ExportFactory;
