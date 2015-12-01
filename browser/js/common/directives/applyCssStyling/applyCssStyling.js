9d78f746b80d82bd5f43426c617528b466643545
Sunil Unka <sunil.unka@gmail.com>
2015-12-01 17:56:27 -0500

    Can apply batch styling inline. Now going to work on creating stylesheet object for page.

--- browser/js/common/directives/applyCssStyling/applyCssStyling.js
+++ browser/js/common/directives/applyCssStyling/applyCssStyling.js
@@ -2,9 +2,6 @@ app.directive("cssApplicator", function(){
   return {
     restrict: 'E',
     templateUrl: '/js/common/directives/applyCssStyling/applyCssStyling.template.html',
-    scope: {
-      newClass: "="
-    },
     link: function(scope, element, attrs){
       scope.newClass = [{ key: "", value: ""}]
       var fieldCounter =  1;
@@ -14,14 +11,25 @@ app.directive("cssApplicator", function(){
         var fieldName = "prop" + fieldCounter;
         scope.newClass.push({ key: "", value: ""});
       }
-      
+
       scope.getCssData = function(data){
-          data.forEach(function(cssObj){
-            console.log(cssObj.key, ":", cssObj.value);
-          })
-          console.log("SCOPE: ", scope.styleGroup)
+        var cssToApply = {};
+        data.forEach(function(cssObj){
+          cssToApply[cssObj.key] = cssObj.value;
+        })
+
+        console.log("CSS TO APPLY OBJECT: ", cssToApply);
+
+        for(var idx in scope.styleGroup){
+          scope.styleGroup[idx].css(cssToApply);
+        }
+
+        
+
+        scope.newClass = [{key: "", value: ""}];
+        scope.styleGroup = {};
       }
-      console.log(scope.newClass);
+      console.log("SCOPE NEW CLASS:", scope.newClass);
     }
   }
 })
