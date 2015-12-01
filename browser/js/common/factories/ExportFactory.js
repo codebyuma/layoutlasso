b4feb00097b70309963c209db6402ac289234ef6
Sarah Edkins <edkins.sarah@gmail.com>
2015-12-01 16:56:37 -0500

    Produces nested html but with some extra - buggy

--- browser/js/common/factories/ExportFactory.js
+++ browser/js/common/factories/ExportFactory.js
@@ -34,48 +34,45 @@ app.factory('ExportFactory', function(GridFactory) {
           visitedGrids[node.parentId].push(node);
         }
       })
-      return visitedGrids;
+      return markGrids(visitedGrids);
     };
 
-// use grid-stack x values to know what is next to what (columns) (or gaps!)
-// use grid-stack y values to group into rows
-// use width to know how width the element is
-// use height to know how tall the element is
+    // mark children nodes that are grids as grids
+    function markGrids(visitedGrids) {
+      var gridNumArray = [];
+      for (var grid in visitedGrids) {
+        gridNumArray.push(grid.slice(4));
+      }
+      GridFactory.savedGrid.forEach(function(node){
+        if (gridNumArray.indexOf(node.id) >= 0) {
+          node.grid = true;
+        }
+      })
+      return visitedGrids;
+    }
 
     ExportFactory.convertToHTML = function() {
+      // TODO integrate save function here to save the current grid
+      // TODO object names can't have hyphens  main-grid => gridmain
       console.log("button clicked!");
-          var html = "";
-          var node;
+          var html = bits.container;
+          console.log("html after just container", html);
           var parentObj = makeParentObject();
-          console.log("parentObj", parentObj);
-          for (var key in parentObj) {  // for each parent
-              // use generateHTML function to build
-              // grid system for each ROW
-              // and each parent grid
-              console.log("parentObj[key]", parentObj[key]);
-              console.log("GridFactory.nestedGrids[key]", GridFactory.nestedGrids[key]);
-              createOffsetNodes(parentObj[key]);
-              generateRow(html, parentObj[key]);
-          }
 
+          for (var key in parentObj){  // modify parentObj to have offset columns
+            createOffsetNodes(parentObj[key]);
+          }
+          // start with main grid.
+          // generateRow function will look for nestedgrids and do the recursion
+          var genRowsHTML =  generateRow(html, parentObj['main-grid'], parentObj);
+          console.log("genRowsHTML is", genRowsHTML);
+
+          html += bits.close; // closes container
+          console.log("*** html from after done in convertToHTML is", html);
       };
 
-  function generateHTML(newHTML, r, c, content, sz){
-    var sz = sz || "md";
-    var rows = r, cols = c;
-    var span = 12 / cols; // number of Bootstrap columns to span across
-    // generate the html
-    newHTML += bits.container;
-    for (var i = 0; i < rows; i++) {
-        newHTML += bits.row;
-          // make row here!!
-        newHTML += bits.close;
-    } // close container
-    newHTML += bits.close;
-  }
-
   function createOffsetNodes(nodesArr) {
-    console.log("nodesArr at beginning of createoffset is", nodesArr);
+    // TODO make offset for end if not up to 12 span
     var curr, nextShouldBe;
     nodesArr.sort(function(a, b){ // make sure nodesArr is sorted by x values
       return a.x - b.x;
@@ -90,25 +87,31 @@ app.factory('ExportFactory', function(GridFactory) {
         nodesArr.splice(i+1, 0, newNode);
       }
     }
-    console.log("nodesArr at end of createoffset is", nodesArr);
-  }
+  };
 
-  function generateRow(html, nodesArr, size) {
+  function generateRow(html, nodesArr, parentObj, size) {
     var sz = size || "md";
+    html += bits.row;
     for (var j = 0; j < nodesArr.length; j++){
         // check if node is an offset node (empty node)
-        console.log("current node is ", nodesArr[j]);
         if (nodesArr[j].offset) {
           html += offsetMaker(sz, nodesArr[j].width);
+        } else if (nodesArr[j].grid) {
+          // find the grid in parent grid and build it
+          console.log("nested grid!!");
+          html += colMaker(sz, nodesArr[j].width); // make column
+          html += nodesArr[j].content; // add content to column
+          console.log("html right before recursive call is", html);
+          html += generateRow(html, parentObj["grid" + nodesArr[j].id], parentObj);
+          html += bits.close; // close nested node's column
         } else {
           html += colMaker(sz, nodesArr[j].width); // make column
           html += nodesArr[j].content;  // add content to column
         }
         html += bits.close; // close column
-    }
-    console.log("html from generateRow() is", html);
+    } html += bits.close; // close row
     return html;
-  }
+  };
 
   return ExportFactory;
 });
