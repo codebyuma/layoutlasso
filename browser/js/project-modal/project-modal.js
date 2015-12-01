09987e522c9ba9025f10d4f5233f5968972fe7d2
Uma Chandran <umachandran6@gmail.com>
2015-12-01 18:29:35 -0500

    working on creating a new project in the project modal controller

--- browser/js/project-modal/project-modal.js
+++ browser/js/project-modal/project-modal.js
@@ -1,14 +1,10 @@
-app.controller('ProjectModalCtrl', function ($scope, $rootScope, user, $uibModalInstance){
+app.controller('ProjectModalCtrl', function ($scope, $rootScope, ProjectFactory, user, $uibModalInstance){
 
   $scope.user = user;
   $scope.projects = user.projects;
   console.log("in project modal, user is", $scope.user) // projects are just ids right now. need to populate?
   console.log("in project modal, user's projects are", $scope.projects);
-  //$scope.projects = $scope.user.
-  // $scope.ok = function(){
-  //   console.log("OK!");
-  //   $uibModalInstance.close()
-  // }
+
 
   $scope.cancel = function(){
   	//console.log("cancel!");
@@ -18,18 +14,35 @@ app.controller('ProjectModalCtrl', function ($scope, $rootScope, user, $uibModal
   $scope.createProject = function (project){
 
     // call create in project factory
+    // CONTINUE HEREEEE
     console.log("in create project:", project);
-    $rootScope.$broadcast('project created', project);
-    $uibModalInstance.close()
+    ProjectFactory.createProject(project.name)
+    .then (function (theProject){
+      $scope.project = theProject;
+      $rootScope.$broadcast('project created', theProject);
+     $uibModalInstance.close()
+    })
+
+
+
+    
 
   }
 
-    $scope.loadProject = function (project){
+  $scope.loadProject = function (project){
 
     // call create in project factory
     console.log("in load project:", project);
-    $rootScope.$broadcast('project loaded', project);
-    $uibModalInstance.close()
+
+    // don't need to call ProjectFactory.getProject as we resolved the projects on the user already
+    // ProjectFactory.getProject(project.id)
+    // .then (function (theProject){
+        $scope.project = project;
+        $rootScope.$broadcast('project loaded', project);
+        $uibModalInstance.close()
+   // })
+
+    
 
   }
 
