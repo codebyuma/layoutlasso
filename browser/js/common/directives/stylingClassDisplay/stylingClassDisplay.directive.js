8272f32a24d2d1a435f74a9a77a223c54912fbd4
Sunil Unka <sunil.unka@gmail.com>
2015-12-02 13:04:22 -0500

    Trying to find cause of random gulp js compile error.

--- browser/js/common/directives/stylingClassDisplay/stylingClassDisplay.directive.js
+++ browser/js/common/directives/stylingClassDisplay/stylingClassDisplay.directive.js
@@ -3,7 +3,6 @@ app.directive("classDisplay", function(StylingFactory){
     restrict: "E",
     templateUrl: "/js/common/directives/stylingClassDisplay/stylingClassDisplay.template.html",
     link: function(scope, element, attrs){
-      
       console.log("PAGE STYLESHEET:", scope);
     }
   }
