f37b39eb6b3e517ba8fb0a47e03aa1ed2704d3e9
Sarah Edkins <edkins.sarah@gmail.com>
2015-12-02 16:42:14 -0500

    trying to make navbar content editable

--- browser/js/common/factories/layout-component.factory.js
+++ browser/js/common/factories/layout-component.factory.js
@@ -2,11 +2,14 @@ app.factory("LayoutComponentFactory",  function($compile){
 
   return {
     addNavBar: function(){
-      return '<nav class="navbar navbar-default">\
+      return '<script> function load(){ window.document.designMode = "On";} </script>\
+      <nav class="navbar navbar-default">\
         <div class="container-fluid">\
           <div class="navbar-header">\
             <a class="navbar-brand" href="#">\
               <p>Navbar</p>\
+              <div contentEditable="true" onload="load()">\
+              This text can be edited by the user.</div>\
             </a>\
           </div>\
         </div>\
