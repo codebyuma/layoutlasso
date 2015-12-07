79e8659daffd179963605a97f54ac2d7b7c61312
Uma Chandran <umachandran6@gmail.com>
2015-12-06 20:49:51 -0500

    trying to fix issue

--- gulpfile.js
+++ gulpfile.js
@@ -43,15 +43,15 @@ gulp.task('lintJS', function () {
 
 });
 
-gulp.task('buildJS', ['lintJS', 'buildBrowserify'], function () {
-    return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
-        .pipe(plumber())
-        .pipe(sourcemaps.init())
-        .pipe(concat('main.js'))
-        .pipe(babel())
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('./public'));
-});
+// gulp.task('buildJS', ['lintJS', 'buildBrowserify'], function () {
+//     return gulp.src(['./browser/js/app.js', './browser/js/**/*.js'])
+//         .pipe(plumber())
+//         .pipe(sourcemaps.init())
+//         .pipe(concat('main.js'))
+//         .pipe(babel())
+//         .pipe(sourcemaps.write())
+//         .pipe(gulp.dest('./public'));
+// });
 
 gulp.task('testServerJS', function () {
     require('babel/register');
