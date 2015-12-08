f104ad8a9a94c1b7c7fcd517dcfda28cfbacd1e4
NicholasJV <nick@Nicks-MBP.local>
2015-12-08 12:32:33 -0500

    cleanup for master merghey

--- browser/js/create-layout/create-layout-controller.js
+++ browser/js/create-layout/create-layout-controller.js
@@ -174,48 +174,8 @@ app.controller("CreateLayoutCtrl", function($scope, AUTH_EVENTS, AuthService, $r
     $scope.showGridManagement = false;
     $scope.toggleGridManagement = function () {
         $scope.showGridManagement = !$scope.showGridManagement;
-        console.log("show grid mgmt: ", $scope.showGridManagement)
         return;
     }
-
-    // $scope.saveGrid = function() {
-    //     $scope.save = true; // flag indicates user has hit save button (used in promptProjectPage to determine if to save the page after loading it)
-    //     NestedStylingFactory.clearNestedStyling(); // Clear any nested styling classes from DOM.
-    //     StyleSaveLoadFactory.removeElementSelectedClassOnSave("lasso-styling-in-progress");
-    //     GridFactory.saveGridLocal(); // save the grid to scope
-    //     if ($scope.user && $rootScope.project && $rootScope.page) {
-    //         GridFactory.saveGridBackend($rootScope.page)
-    //     } else {
-    //         if (!$scope.user) {
-    //             $scope.promptUserLogin();
-    //             ModalFactory.userLoginModal.result.then(function(user) {
-    //                 $scope.user = user;
-    //                 if (!$rootScope.project) {
-    //                     $scope.promptProjectLoad();
-    //                 }
-    //             })
-    //         } else {
-    //             $scope.promptProjectLoad();
-    //         }
-    //     }
-    //     if($scope.stylingModeActive){
-    //       NestedStylingFactory.findEditableLayer($("#main-grid"), ".grid-stack-item");
-    //     }
-    // }
-
-    // $scope.clearGrid = function(){
-    //   StyleModeFactory.deactivateStyleMode($scope);
-    //   GridFactory.clearGrid();
-    //   $scope.nestedGrids = GridFactory.getNestedGrids();
-    //   $scope.pageStyleSheet = [];
-    // }
-
-    // $scope.loadGrid = function() {
-    //     GridFactory.loadGrid($scope, $rootScope.page);
-    //     $scope.nestedGrids = GridFactory.getNestedGrids();
-    //     $scope.pageStyleSheet = StylingFactory.getStyleSheetClassNames();
-    //     if($scope.styleModeActive) NestedStylingFactory.findEditableLayer();
-    // }
 
 
     //===== Templates ===== //
